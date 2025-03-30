498038fbb6aea2a08cfffe187e1c98e531deb974
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-03-30 11:36:06 +0700

    membuat fungsi edit dan update, dan view edit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,9 +7,11 @@ use App\Models\Product;
 use Illuminate\Http\RedirectResponse;
 // import Http Request
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Redirect;
 // import return type view
 use Illuminate\View\View; 
+// import Facedes Storage
+use Illuminate\Support\Facades\Storage;
 
 
 class ProductController extends Controller
@@ -86,5 +88,65 @@ class ProductController extends Controller
 
         // render view with product
         return view('products.show', compact('product', 'title'));
+    }
+
+    public function edit(string $id) : View {
+        $title = 'Edit Product';
+        $product = Product::findOrFail($id);
+
+        return view('products.edit', compact('title', 'product'));
+    }
+
+    /**
+     * 
+     * update
+     * @param mixed $request
+     * @param mixed $id
+     * @return RedirectResponse
+     */
+
+    public function update(Request $request, $id) : RedirectResponse {
+         // validate form
+        $request->validate([
+            'image'         => 'image|mimes:jpeg,jpg,png|max:2048',
+            'title'         => 'required|min:5',
+            'description'   => 'required|min:10',
+            'price'         => 'required|numeric',
+            'stock'         => 'required|numeric'
+        ]);
+
+        // get product by id
+        $product = Product::findOrFail($id);
+
+        // check if image is uploaded
+        if ($request->hasFile('image')) {
+            // delete old image
+            Storage::delete('products/'.$product->image);
+
+            // upload new image
+            $image = $request->file('image');
+            $image->storeAs('products', $image->hashName());
+
+            // update product with new image
+            $product->update([
+                'image'         => $image->hashName(),
+                'title'         => $request->title,
+                'description'   => $request->description,
+                'price'         => $request->price,
+                'stock'         => $request->stock
+            ]);
+
+        } else {
+            // update product without image
+            $product->update([
+                'image' => $request->title,
+                'description' => $request->description,
+                'price' => $request->price,
+                'stock' => $request->stock
+            ]);
+        }
+
+        // redirect to index
+        return redirect()->route('products.index')->with(['success' => 'Data Berhasil Diupdate']);
     }
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,20 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body style="background: lightgray">
+
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-12">
                 <div>
-                    <h3 class="text-center my-4">Tutorial Laravel 12</h3>
+                    <h3 class="text-center my-4">Tutorial Laravel 12 untuk Pemula</h3>
                     <h5 class="text-center"><a href="https://santrikoding.com">www.santrikoding.com</a></h5>
                     <hr>
                 </div>
                 <div class="card border-0 shadow-sm rounded">
                     <div class="card-body">
-                        <a href="{{ route('products.create') }}" class="btn btn-md btn-success mb-3">Add Product</a>
+                        <a href="{{ route('products.create') }}" class="btn btn-md btn-success mb-3">ADD PRODUCT</a>
                         <table class="table table-bordered">
                             <thead class="text-center">
                                 <tr>
@@ -33,40 +34,40 @@
                                 @forelse ($products as $product)
                                     <tr>
                                         <td class="text-center">
-                                            <img src="{{ asset('/storage/products/'.$product->image) }}" class="rounded" alt="" style="width: 150px">
+                                            <img src="{{ asset('/storage/products/'.$product->image) }}" class="rounded" style="width: 150px">
                                         </td>
                                         <td>{{ $product->title }}</td>
                                         <td>{{ "Rp " . number_format($product->price,2,',','.') }}</td>
                                         <td>{{ $product->stock }}</td>
                                         <td class="text-center">
-                                            <form onsubmit="return confirm('Apakah Anda Yakin?');" action="{{ route('products.destroy', $product->id) }}" method="POST"></form>
-                                            <a href="{{ route('products.show', $product->id) }}" class="btn btn-sm btn-secondary">SHOW</a>
-                                             <a href="{{--{{ route('products.edit') }}--}}" class="btn btn-sm btn-primary">EDIT</a> 
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
+                                            <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('products.destroy', $product->id) }}" method="POST">
+                                                <a href="{{ route('products.show', $product->id) }}" class="btn btn-sm btn-secondary">SHOW</a>
+                                                <a href="{{ route('products.edit', $product->id) }}" class="btn btn-sm btn-primary">EDIT</a>
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
+                                            </form>
                                         </td>
                                     </tr>
                                 @empty
                                     <div class="alert alert-danger">
-                                        Data Products belum ada
+                                        Data Products belum ada.
                                     </div>
                                 @endforelse
                             </tbody>
                         </table>
                         {{ $products->links() }}
                     </div>
-
                 </div>
             </div>
         </div>
     </div>
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <script>
-        // message with sweetalert
+        //message with sweetalert
         @if(session('success'))
             Swal.fire({
                 icon: "success",
@@ -84,6 +85,8 @@
                 timer: 2000
             });
         @endif
+
     </script>
+
 </body>
 </html>
